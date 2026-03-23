970674521c541e364efe3888d94b0cf13a1fde39
Sung hun kwag <speedkjr13@naver.com>
2026-03-23 20:31:17 +0900

    Initialize rsi_bench module with version and author

---
+++ rsi_bench/__init__.py
@@ -0,0 +1,11 @@
+"""
+RSI-Bench: Multi-Axis Benchmark for Recursive Self-Improvement
+"""
+
+__version__ = "0.1.0"
+__author__ = "Sung Hun Kwag"
+
+from rsi_bench.core import RSIBenchmark
+from rsi_bench.scoring import UnifiedScorer
+
+__all__ = ["RSIBenchmark", "UnifiedScorer"]
